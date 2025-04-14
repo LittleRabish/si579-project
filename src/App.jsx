5863641612b9ea50efb08b8e98061a63dc88911e
LittleRabish <muyanh@umich.edu>
2025-04-14 04:45:05 -0400

    Fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Map from './components/map.jsx';
+import Map from './components/Map.jsx';
 
 function App() {
   const [latLng, setLatLng] = useState(null);
