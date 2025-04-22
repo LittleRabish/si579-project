b2bc2fbf1d588be7b36d725bb04fa9045ff010dd
LittleRabish <muyanh@umich.edu>
2025-04-22 01:00:32 -0400

    Testing the popup image of Leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,16 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
 import LocationMarker from './LocationMarker.jsx';
 
 const Map = ({ latLng, handleMapClick }) => {
-    return (
-        <MapContainer center={[42.28, -83.73]} zoom={13} style={{ height: '500px' }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <LocationMarker latLng={latLng} handleMapClick={handleMapClick} />
-        </MapContainer>
-    );
+  return (
+    <MapContainer center={[42.28, -83.73]} zoom={13} style={{ height: '500px' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker latLng={latLng} handleMapClick={handleMapClick} />
+    </MapContainer>
+  );
 };
 
 export default Map;
